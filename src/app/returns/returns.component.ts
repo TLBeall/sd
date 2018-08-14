80e27789abf66e81bf7f356fb68f300ec71bc20f
Sherif Mankarious <smankarious@sunrisedataservices.com>
2018-08-14 14:31:35 -0400

    Child Rows

--- src/app/returns/returns.component.ts
+++ src/app/returns/returns.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ContentChild } from '@angular/core';
 import { AuthService } from '../Services/auth.service';
 import { delay, share } from 'rxjs/operators';
 import { RootReturns } from '../Models/RootReturns';
@@ -13,15 +13,21 @@ import * as $ from 'jquery';
 })
 export class ReturnsComponent implements OnInit {
 
+  @ViewChild(MailtypeReturnsComponent, {read: ElementRef})
+  public mailTypeChild: ElementRef;
   public RootReturns : RootReturns;
   public tableWidget: any;
   constructor(private _authService: AuthService) {     
   }
 
   ngOnInit() {
+    $("#returnsDiv").toggle(false);
+    $(".clientTable").toggle(false);
     this._authService.getReturns()
     .subscribe(data => {
       this.RootReturns = data;
+      $("#returnsDiv").toggle(true);
+      $(".clientTable").toggle(true);
     });  
   }
 
@@ -30,9 +36,30 @@ export class ReturnsComponent implements OnInit {
   }
 
   private initDatatable(): void {
-    let exampleId: any = $('#example');
-    this.tableWidget = exampleId.DataTable({
-      select: true
+    let clientTable: any = $('table.clientTable');
+    this.tableWidget = clientTable.DataTable({
+      select: true,
+      autoWidth: false,
+      paging: false,
+      info: false,
+      searching: false,
+      initComplete: function () {
+      }
+    });
+
+    var el: HTMLElement = this.mailTypeChild.nativeElement;
+    var childTable = el.getElementsByClassName('mailTypeTable');
+     this.tableWidget.rows().every(function () {
+       var tableHeader = $('.clientTableHeader');
+       var tr = $(this.node());
+       var row = this.row(tr);  
+       row.child(childTable[0]).show();
+       // tableHeader.css("visibility", "collapse");
+       for (var j = 2; j < 25; j++) {
+               var nodes = this.column(j).nodes();
+               $(nodes[row.index()]).addClass('hideParentRow');
+           }
+       tr.addClass('shown');
     });
   }
 }

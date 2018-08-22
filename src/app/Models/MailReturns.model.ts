634510c407f0e77ac82f72c5f94a8a85c111f8a3
Tyler <tbeall@sunrisedataservices.com>
2018-08-22 12:03:05 -0400

    <Tyler>

--- src/app/Models/MailReturns.model.ts
+++ src/app/Models/MailReturns.model.ts
@@ -3,21 +3,14 @@ import { Returns } from "./Returns.model";
 export class MailReturns {
     MailCode: string;
     MailDescription: string;
-<<<<<<< HEAD:src/app/Models/MailReturns.ts
-=======
     ExchangeFlag: string;
     ListOwner: number;
     ListOwnerName: string;
     ListManagerName: string;
->>>>>>> 410a2a91437176135598952c34616ab86897287e:src/app/Models/MailReturns.model.ts
     ListManager: number;
     ListManagerAbr: string;
-    ListManagerName: string;
-    ListOwner: number;
-    ListOwnerName: string;
     ListOwnerAbr: string;
     Recency: number;
     RecencyString: string;
     Measure: Returns;
-    ExchangeFlag: string;
 }

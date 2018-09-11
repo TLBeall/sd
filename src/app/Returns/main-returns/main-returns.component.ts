0214ca2d0de5b0f1a53bdf731ca3f6a5e99dc2b3
Sherif Mankarious <smankarious@sunrisedataservices.com>
2018-09-11 10:57:58 -0400

    grouping

--- src/app/Returns/main-returns/main-returns.component.ts
+++ src/app/Returns/main-returns/main-returns.component.ts
@@ -18,35 +18,65 @@ export class ReturnsComponent  {
 
   private gridApi;
   private gridColumnApi;
-  private rowData: any;
+  private rowData: any[];
   private service: AuthService;
-  private MailTypeRender;
-  private CampaignRender;
-  private PhaseRender;
-  private MailRender;
   private activeClient;
   private selectedYear;
 
+  // private gridApi;
+  // private gridColumnApi;
+  // private rowData: any;
+  // private service: AuthService;
+  // private MailTypeRender;
+  // private CampaignRender;
+  // private PhaseRender;
+  // private MailRender;
+  // private activeClient;
+  // private selectedYear;
+
   private columnDefs = [
-    { headerName: 'Client', field: 'Client', cellRenderer: "agGroupCellRenderer"},
-    { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
-      return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) } },
-    { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
-      return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-    { headerName: 'Quantity', field: 'Measure.Quantity'},
-    { headerName: 'Donors', field: 'Measure.Donors'},
-    { headerName: 'NonDonors', field: 'Measure.NonDonors'},
-    { headerName: 'NewDonors', field: 'Measure.NewDonors'},
-    { headerName: 'RSP', field: 'Measure.RSP'},
-    { headerName: 'AVG', field: 'Measure.AVG'},
-    { headerName: 'Gross', field: 'Measure.Gross'},
-    { headerName: 'Cost', field: 'Measure.Cost'},
-    { headerName: 'Net', field: 'Measure.Net'},
-    { headerName: 'GPP', field: 'Measure.GPP'},
-    { headerName: 'CLM', field: 'Measure.CLM'},
-    { headerName: 'NLM', field: 'Measure.NLM'},
-    { headerName: 'IO', field: 'Measure.IO'}
+    { headerName: 'Mail type', field: 'MailType', rowGroup: true },
+    { headerName: 'Campaign', field: 'CampaignName', rowGroup: true },
+    { headerName: 'Phase', field: 'PhaseName', rowGroup: true },
+    { headerName: 'Mail Code', field: 'MailCode' },
+    { headerName: 'Description', field: 'MailDescription' },
+    // { headerName: 'Mailed', field: 'Mailed'},
+    // { headerName: 'Caged', field: 'Caged'},
+    { headerName: 'Quantity', field: 'Quantity'}
+    // { headerName: 'Donors', field: 'Donors'},
+    // { headerName: 'NonDonors', field: 'NonDonors'},
+    // { headerName: 'NewDonors', field: 'NewDonors'},
+    // { headerName: 'RSP', field: 'RSP'},
+    // { headerName: 'AVG', field: 'AVG'},
+    // { headerName: 'Gross', field: 'Gross'},
+    // { headerName: 'Cost', field: 'Cost'},
+    // { headerName: 'Net', field: 'Net'},
+    // { headerName: 'GPP', field: 'GPP'},
+    // { headerName: 'CLM', field: 'CLM'},
+    // { headerName: 'NLM', field: 'NLM'},
+    // { headerName: 'IO', field: 'IO'}
   ];
+
+  // private columnDefs = [
+  //   { headerName: 'Client', field: 'Client', cellRenderer: "agGroupCellRenderer"},
+  //   { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
+  //     return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) } },
+  //   { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
+  //     return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //   { headerName: 'Quantity', field: 'Measure.Quantity'},
+  //   { headerName: 'Donors', field: 'Measure.Donors'},
+  //   { headerName: 'NonDonors', field: 'Measure.NonDonors'},
+  //   { headerName: 'NewDonors', field: 'Measure.NewDonors'},
+  //   { headerName: 'RSP', field: 'Measure.RSP'},
+  //   { headerName: 'AVG', field: 'Measure.AVG'},
+  //   { headerName: 'Gross', field: 'Measure.Gross'},
+  //   { headerName: 'Cost', field: 'Measure.Cost'},
+  //   { headerName: 'Net', field: 'Measure.Net'},
+  //   { headerName: 'GPP', field: 'Measure.GPP'},
+  //   { headerName: 'CLM', field: 'Measure.CLM'},
+  //   { headerName: 'NLM', field: 'Measure.NLM'},
+  //   { headerName: 'IO', field: 'Measure.IO'}
+  // ];
 
   constructor(service: AuthService, route: ActivatedRoute, private loaderService: LoaderService) {
     this.service = service;
@@ -54,137 +84,138 @@ export class ReturnsComponent  {
       this.activeClient = params["client"]; 
       this.selectedYear = params["year"];
     });
-
-    this.MailRender = {
-      detailGridOptions: {
-        columnDefs: [
-          { headerName: 'Code', field: "MailCode"},
-          { headerName: 'Description', field: "MailDescription" },
-          { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Quantity', field: 'Measure.Quantity'},
-          { headerName: 'Donors', field: 'Measure.Donors'},
-          { headerName: 'NonDonors', field: 'Measure.NonDonors'},
-          { headerName: 'NewDonors', field: 'Measure.NewDonors'},
-          { headerName: 'RSP', field: 'Measure.RSP'},
-          { headerName: 'AVG', field: 'Measure.AVG'},
-          { headerName: 'Gross', field: 'Measure.Gross'},
-          { headerName: 'Cost', field: 'Measure.Cost'},
-          { headerName: 'Net', field: 'Measure.Net'},
-          { headerName: 'GPP', field: 'Measure.GPP'},
-          { headerName: 'CLM', field: 'Measure.CLM'},
-          { headerName: 'NLM', field: 'Measure.NLM'},
-          { headerName: 'IO', field: 'Measure.IO'}          
-        ],
-        enableSorting: true
-      },    
-      getDetailRowData: function(params) {
-        params.successCallback(params.data.MailList);
       }
-    };    
 
-    this.PhaseRender = {
-      detailGridOptions: {
-        columnDefs: [
-          { headerName: 'Phase', field: "PhaseName", cellRenderer: "agGroupCellRenderer" },
-          { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Quantity', field: 'Measure.Quantity'},
-          { headerName: 'Donors', field: 'Measure.Donors'},
-          { headerName: 'NonDonors', field: 'Measure.NonDonors'},
-          { headerName: 'NewDonors', field: 'Measure.NewDonors'},
-          { headerName: 'RSP', field: 'Measure.RSP'},
-          { headerName: 'AVG', field: 'Measure.AVG'},
-          { headerName: 'Gross', field: 'Measure.Gross'},
-          { headerName: 'Cost', field: 'Measure.Cost'},
-          { headerName: 'Net', field: 'Measure.Net'},
-          { headerName: 'GPP', field: 'Measure.GPP'},
-          { headerName: 'CLM', field: 'Measure.CLM'},
-          { headerName: 'NLM', field: 'Measure.NLM'},
-          { headerName: 'IO', field: 'Measure.IO'}          
-        ],
-        enableSorting: true,
-        masterDetail: true,  
-        groupDefaultExpanded: -1, 
-        detailCellRendererParams: this.MailRender
-      },    
-      getDetailRowData: function(params) {
-        params.successCallback(params.data.PhaseList);
-      }
-    };
+  //   this.MailRender = {
+  //     detailGridOptions: {
+  //       columnDefs: [
+  //         { headerName: 'Code', field: "MailCode"},
+  //         { headerName: 'Description', field: "MailDescription" },
+  //         { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Quantity', field: 'Measure.Quantity'},
+  //         { headerName: 'Donors', field: 'Measure.Donors'},
+  //         { headerName: 'NonDonors', field: 'Measure.NonDonors'},
+  //         { headerName: 'NewDonors', field: 'Measure.NewDonors'},
+  //         { headerName: 'RSP', field: 'Measure.RSP'},
+  //         { headerName: 'AVG', field: 'Measure.AVG'},
+  //         { headerName: 'Gross', field: 'Measure.Gross'},
+  //         { headerName: 'Cost', field: 'Measure.Cost'},
+  //         { headerName: 'Net', field: 'Measure.Net'},
+  //         { headerName: 'GPP', field: 'Measure.GPP'},
+  //         { headerName: 'CLM', field: 'Measure.CLM'},
+  //         { headerName: 'NLM', field: 'Measure.NLM'},
+  //         { headerName: 'IO', field: 'Measure.IO'}          
+  //       ],
+  //       enableSorting: true
+  //     },    
+  //     getDetailRowData: function(params) {
+  //       params.successCallback(params.data.MailList);
+  //     }
+  //   };    
 
-    this.CampaignRender = {
-      detailGridOptions: {
-        columnDefs: [
-          { headerName: 'Campaign', field: "CampaignName", cellRenderer: "agGroupCellRenderer" },
-          { headerName: 'Mailed', field: 'Measure.Mailed', cellRenderer: function(data) { return '<div _ngcontent-c4=""> Test Click...</div>' + data.value + '</b></div>'}, valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Quantity', field: 'Measure.Quantity'},
-          { headerName: 'Donors', field: 'Measure.Donors'},
-          { headerName: 'NonDonors', field: 'Measure.NonDonors'},
-          { headerName: 'NewDonors', field: 'Measure.NewDonors'},
-          { headerName: 'RSP', field: 'Measure.RSP'},
-          { headerName: 'AVG', field: 'Measure.AVG'},
-          { headerName: 'Gross', field: 'Measure.Gross'},
-          { headerName: 'Cost', field: 'Measure.Cost'},
-          { headerName: 'Net', field: 'Measure.Net'},
-          { headerName: 'GPP', field: 'Measure.GPP'},
-          { headerName: 'CLM', field: 'Measure.CLM'},
-          { headerName: 'NLM', field: 'Measure.NLM'},
-          { headerName: 'IO', field: 'Measure.IO'}          
-        ],
-        enableSorting: true,   
-        masterDetail: true,     
-        groupDefaultExpanded: -1,           
-        detailCellRendererParams: this.PhaseRender
-      },    
-      getDetailRowData: function(params) {
-        params.successCallback(params.data.CampaignList);
-      }
-    };
+  //   this.PhaseRender = {
+  //     detailGridOptions: {
+  //       columnDefs: [
+  //         { headerName: 'Phase', field: "PhaseName", cellRenderer: "agGroupCellRenderer" },
+  //         { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Quantity', field: 'Measure.Quantity'},
+  //         { headerName: 'Donors', field: 'Measure.Donors'},
+  //         { headerName: 'NonDonors', field: 'Measure.NonDonors'},
+  //         { headerName: 'NewDonors', field: 'Measure.NewDonors'},
+  //         { headerName: 'RSP', field: 'Measure.RSP'},
+  //         { headerName: 'AVG', field: 'Measure.AVG'},
+  //         { headerName: 'Gross', field: 'Measure.Gross'},
+  //         { headerName: 'Cost', field: 'Measure.Cost'},
+  //         { headerName: 'Net', field: 'Measure.Net'},
+  //         { headerName: 'GPP', field: 'Measure.GPP'},
+  //         { headerName: 'CLM', field: 'Measure.CLM'},
+  //         { headerName: 'NLM', field: 'Measure.NLM'},
+  //         { headerName: 'IO', field: 'Measure.IO'}          
+  //       ],
+  //       enableSorting: true,
+  //       masterDetail: true,  
+  //       groupDefaultExpanded: -1, 
+  //       detailCellRendererParams: this.MailRender
+  //     },    
+  //     getDetailRowData: function(params) {
+  //       params.successCallback(params.data.PhaseList);
+  //     }
+  //   };
 
-    this.MailTypeRender = {
-      detailGridOptions: {
-        columnDefs: [
-          { headerName: 'Mail Type', field: "MailType", cellRenderer: "agGroupCellRenderer" },
-          { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
-            return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
-          { headerName: 'Quantity', field: 'Measure.Quantity'},
-          { headerName: 'Donors', field: 'Measure.Donors'},
-          { headerName: 'NonDonors', field: 'Measure.NonDonors'},
-          { headerName: 'NewDonors', field: 'Measure.NewDonors'},
-          { headerName: 'RSP', field: 'Measure.RSP'},
-          { headerName: 'AVG', field: 'Measure.AVG'},
-          { headerName: 'Gross', field: 'Measure.Gross'},
-          { headerName: 'Cost', field: 'Measure.Cost'},
-          { headerName: 'Net', field: 'Measure.Net'},
-          { headerName: 'GPP', field: 'Measure.GPP'},
-          { headerName: 'CLM', field: 'Measure.CLM'},
-          { headerName: 'NLM', field: 'Measure.NLM'},
-          { headerName: 'IO', field: 'Measure.IO'}          
-        ],
-        enableSorting: true,   
-        masterDetail: true, 
-        groupDefaultExpanded: -1,     
-        detailCellRendererParams: this.CampaignRender
-      },    
-      getDetailRowData: function(params) {
-        params.successCallback(params.data.MailTypeList);
-      }
-    };
-  }
+  //   this.CampaignRender = {
+  //     detailGridOptions: {
+  //       columnDefs: [
+  //         { headerName: 'Campaign', field: "CampaignName", cellRenderer: "agGroupCellRenderer" },
+  //         { headerName: 'Mailed', field: 'Measure.Mailed', cellRenderer: function(data) { return '<div _ngcontent-c4=""> Test Click...</div>' + data.value + '</b></div>'}, valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Quantity', field: 'Measure.Quantity'},
+  //         { headerName: 'Donors', field: 'Measure.Donors'},
+  //         { headerName: 'NonDonors', field: 'Measure.NonDonors'},
+  //         { headerName: 'NewDonors', field: 'Measure.NewDonors'},
+  //         { headerName: 'RSP', field: 'Measure.RSP'},
+  //         { headerName: 'AVG', field: 'Measure.AVG'},
+  //         { headerName: 'Gross', field: 'Measure.Gross'},
+  //         { headerName: 'Cost', field: 'Measure.Cost'},
+  //         { headerName: 'Net', field: 'Measure.Net'},
+  //         { headerName: 'GPP', field: 'Measure.GPP'},
+  //         { headerName: 'CLM', field: 'Measure.CLM'},
+  //         { headerName: 'NLM', field: 'Measure.NLM'},
+  //         { headerName: 'IO', field: 'Measure.IO'}          
+  //       ],
+  //       enableSorting: true,   
+  //       masterDetail: true,     
+  //       groupDefaultExpanded: -1,           
+  //       detailCellRendererParams: this.PhaseRender
+  //     },    
+  //     getDetailRowData: function(params) {
+  //       params.successCallback(params.data.CampaignList);
+  //     }
+  //   };
+
+  //   this.MailTypeRender = {
+  //     detailGridOptions: {
+  //       columnDefs: [
+  //         { headerName: 'Mail Type', field: "MailType", cellRenderer: "agGroupCellRenderer" },
+  //         { headerName: 'Mailed', field: 'Measure.Mailed', valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Caged', field: 'Measure.Caged', valueFormatter: function(data) {
+  //           return data.value.substring(5,7) + '/' + data.value.substring(8,10) + '/' + data.value.substring(0,4) }},
+  //         { headerName: 'Quantity', field: 'Measure.Quantity'},
+  //         { headerName: 'Donors', field: 'Measure.Donors'},
+  //         { headerName: 'NonDonors', field: 'Measure.NonDonors'},
+  //         { headerName: 'NewDonors', field: 'Measure.NewDonors'},
+  //         { headerName: 'RSP', field: 'Measure.RSP'},
+  //         { headerName: 'AVG', field: 'Measure.AVG'},
+  //         { headerName: 'Gross', field: 'Measure.Gross'},
+  //         { headerName: 'Cost', field: 'Measure.Cost'},
+  //         { headerName: 'Net', field: 'Measure.Net'},
+  //         { headerName: 'GPP', field: 'Measure.GPP'},
+  //         { headerName: 'CLM', field: 'Measure.CLM'},
+  //         { headerName: 'NLM', field: 'Measure.NLM'},
+  //         { headerName: 'IO', field: 'Measure.IO'}          
+  //       ],
+  //       enableSorting: true,   
+  //       masterDetail: true, 
+  //       groupDefaultExpanded: -1,     
+  //       detailCellRendererParams: this.CampaignRender
+  //     },    
+  //     getDetailRowData: function(params) {
+  //       params.successCallback(params.data.MailTypeList);
+  //     }
+  //   };
+  // }
 
   onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
-    this.rowData = this.service.getReturns(this.activeClient, new Date("01/01/" + this.selectedYear), new Date("12/31/" + this.selectedYear));
+    this.rowData = this.service.getNewReturns(this.activeClient, new Date("01/01/" + this.selectedYear), new Date("12/31/" + this.selectedYear));
   }
 }

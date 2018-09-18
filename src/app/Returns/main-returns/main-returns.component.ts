48d879d8f7f18d8b12dd880baff90fa1ed2ac8bf
Sherif Mankarious <smankarious@sunrisedataservices.com>
2018-09-18 17:16:17 -0400

    update changes

--- src/app/Returns/main-returns/main-returns.component.ts
+++ src/app/Returns/main-returns/main-returns.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Sort} from '@angular/material';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Sort } from '@angular/material';
 import { LoaderService } from '../../Loader/loader.service';
 import { ActivatedRoute } from "@angular/router";
 import { animate, state, style, transition, trigger } from '@angular/animations';
@@ -11,8 +11,8 @@ import { RootReturns } from '../../Models/RootReturns.model';
   styleUrls: ['./main-returns.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ]
@@ -26,59 +26,78 @@ export class ReturnsComponent {
   private valueToReturn: boolean = false;
   private TempList: any;
   private RootDataSource: any;
+  private ChangeDetector: any;
 
-  clientDisplayedColumns: string[] = ['Expand','Client', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
-  mailTypeDisplayedColumns: string[] = ['Expand','MailType', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
-  campaignDisplayedColumns: string[] = ['Expand','CampaignName', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
-  phaseDisplayedColumns: string[] = ['Expand','PhaseName', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
+  clientDisplayedColumns: string[] = ['Expand', 'Client', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
+  mailTypeDisplayedColumns: string[] = ['Expand', 'MailType', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
+  campaignDisplayedColumns: string[] = ['Expand', 'CampaignName', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
+  phaseDisplayedColumns: string[] = ['Expand', 'PhaseName', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
   mailListDisplayedColumns: string[] = ['MailCode', 'Mailed', 'Caged', 'Quantity', 'Donors', 'NonDonors', 'NewDonors', 'RSP', 'AVG', 'Gross', 'Cost', 'Net', 'GPP', 'CLM', 'NLM', 'IO'];
 
-  constructor(route: ActivatedRoute) {
+  constructor(route: ActivatedRoute, private changeDetectorRefs: ChangeDetectorRef) {
     this.route = route;
-  }  
+    this.ChangeDetector = changeDetectorRefs;
+  }
 
   ngOnInit() {
     this.rowData = this.route.snapshot.data['rowData'];
-    // this.rowData[0].MailTypeList.forEach(a => 
-    //   { 
-    //     a.CampaignList.forEach(b => 
-    //     { 
-    //       b.PhaseList.forEach(c => 
-    //         { c.MailDataSource = new MatTableDataSource(c.MailList); 
-    //         });
-    //         b.PhaseDataSource = new MatTableDataSource(b.PhaseList);
-    //     });
-    //     a.CampaingDataSource = new MatTableDataSource(a.CampaignList); 
-    //   });
-    //   this.rowData[0].MailTypeDataSource = new MatTableDataSource(this.rowData[0].MailTypeList);
-    //   this.rowData[0].MailTypeDataSource.sort = this.mailTypeSort;
-    }
+  }
 
-  GetVisibilityStyle(state: boolean): string
-  {
+  GetVisibilityStyle(state: boolean): string {
     if (state)
       return 'visible';
     return 'collapse';
   }
 
-  CollapseNextLevel(Parent: any, ChildList: any[])
-  {
-    ChildList.forEach(a =>  {
+  CollapseNextLevel(Parent: any, ChildList: any[]) {
+    ChildList.forEach(a => {
       if (a.Measure.Expanded == true)
         a.Measure.Expanded = false;
     })
 
     if (Parent.Measure.Expanded == false)
       Parent.Measure.Expanded = true;
-   }
+  }
 
-   ToggleExpansion(Element: any)
-   {
-     Element.Measure.Expanded = ! Element.Measure.Expanded;
-   }
+  ToggleExpansion(Element: any) {
+    Element.Measure.Expanded = !Element.Measure.Expanded;
+  }
 
-   MailTypeSort(sort: Sort, mailTypeList: any){
-    this.rowData[0].MailTypeList[0] = this.rowData[0].MailTypeList[1];
-   }
+  MailTypeSort(sort: Sort, mailTypeList: any) {
+
+    const data = this.rowData[0].MailTypeList.slice();
+
+    if (!sort.active || sort.direction === '') {
+      sort.direction = "asc";
+      sort.active = "MailType";
+      this.rowData[0].MailTypeList = data;
+    }
+
+    this.rowData[0].MailTypeList = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'MailType': return compare(a.MailType, b.MailType, isAsc);
+        case 'Mailed': return compare(a.Measure.Mailed, b.Measure.Mailed, isAsc);
+        case 'Caged': return compare(a.Measure.Caged, b.Measure.Caged, isAsc);
+        case 'Quantity': return compare(a.Measure.Quantity, b.Measure.Quantity, isAsc);
+        case 'Donors': return compare(a.Measure.Donors, b.Measure.Donors, isAsc);
+        case 'NonDonors': return compare(a.Measure.NonDonors, b.Measure.NonDonors, isAsc);
+        case 'NewDonors': return compare(a.Measure.NewDonors, b.Measure.NewDonors, isAsc);
+        case 'RSP': return compare(a.Measure.RSP, b.Measure.RSP, isAsc);
+        case 'AVG': return compare(a.Measure.AVG, b.Measure.AVG, isAsc);
+        case 'Gross': return compare(a.Measure.Gross, b.Measure.Gross, isAsc);
+        case 'Cost': return compare(a.Measure.Cost, b.Measure.Cost, isAsc);
+        case 'Net': return compare(a.Measure.Net, b.Measure.Net, isAsc);
+        case 'GPP': return compare(a.Measure.GPP, b.Measure.GPP, isAsc);
+        case 'CLM': return compare(a.Measure.CLM, b.Measure.CLM, isAsc);
+        case 'NLM': return compare(a.Measure.NLM, b.Measure.NLM, isAsc);
+        case 'IO': return compare(a.Measure.IO, b.Measure.IO, isAsc);
+        default: return 0;
+      }
+    });
+  }
 }
 
+function compare(a: string, b: string, isAsc) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}

c1330f52ecafb8977584611432a1bdb07a7573b7
Sherif Mankarious <smankarious@sunrisedataservices.com>
2018-09-19 12:49:40 -0400

    updates

--- src/app/Returns/main-returns/main-returns.component.ts
+++ src/app/Returns/main-returns/main-returns.component.ts
@@ -11,7 +11,7 @@ import { RootReturns } from '../../Models/RootReturns.model';
   styleUrls: ['./main-returns.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('collapsed, void', style({ height: '0px', minHeight: '0', display: 'none' })),
       state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
@@ -49,11 +49,48 @@ export class ReturnsComponent {
     return 'collapse';
   }
 
-  CollapseNextLevel(Parent: any, ChildList: any[]) {
-    ChildList.forEach(a => {
-      if (a.Measure.Expanded == true)
-        a.Measure.Expanded = false;
+  ReadyToCollapseAll(list: any[]): boolean {
+    var RetValue: boolean = false;
+    list.forEach(b => {
+      if (b.Measure.Expanded == true)
+      {
+        RetValue = true;
+        return RetValue;
+      }
     })
+    return RetValue;
+  }
+
+  NextLevel(Parent: any, ChildList: any[]) {
+    var SetToExpand = true;
+    if (this.ReadyToCollapseAll(ChildList))
+      SetToExpand = false;
+    if (Parent.MailTypeList != null) {
+      ChildList.forEach(a => {
+        a.CampaignList.forEach(b => {
+          b.PhaseList.forEach(c => {
+            c.Measure.Expanded = SetToExpand;
+          })
+          b.Measure.Expanded = SetToExpand;
+        })
+        a.Measure.Expanded = SetToExpand;
+      })
+    }
+
+    if (Parent.CampaignList != null) {
+      ChildList.forEach(a => {
+        a.PhaseList.forEach(c => {
+          c.Measure.Expanded = SetToExpand;
+        })
+        a.Measure.Expanded = SetToExpand;
+      })
+    }
+
+    if (Parent.PhaseList != null) {
+      ChildList.forEach(a => {
+        a.Measure.Expanded = SetToExpand;
+      })
+    }
 
     if (Parent.Measure.Expanded == false)
       Parent.Measure.Expanded = true;
@@ -63,20 +100,77 @@ export class ReturnsComponent {
     Element.Measure.Expanded = !Element.Measure.Expanded;
   }
 
-  MailTypeSort(sort: Sort, mailTypeList: any) {
+  SortFunction(sort: Sort, Element: any) {
 
-    const data = this.rowData[0].MailTypeList.slice();
+    var data: any;
+    var myType: string = "";
+
+    if (Element.CampaignList != null) myType = "MailType";
+    if (Element.PhaseList != null) myType = "Campaign";
+    if (Element.MailList != null) myType = "Phase";
+    if (myType == "") myType = "MailTypeList";
+
+    switch (myType) {
+      case "MailTypeList": {
+        if (this.ReadyToCollapseAll(this.rowData[0].MailTypeList))
+          this.NextLevel(this.rowData[0], this.rowData[0].MailTypeList);
+        data = this.rowData[0].MailTypeList.slice();
+        break;
+      }
+      case "MailType": {
+        if (this.ReadyToCollapseAll(Element.CampaignList))
+          this.NextLevel(Element, Element.CampaignList);
+        data = Element.CampaignList.slice();
+        break;
+      }
+      case "Campaign": {
+        if (this.ReadyToCollapseAll(Element.PhaseList))
+          this.NextLevel(Element, Element.PhaseList);
+        data = Element.PhaseList.slice();
+        break;
+      }
+      case "Phase": {
+        data = Element.MailList.slice();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
 
     if (!sort.active || sort.direction === '') {
       sort.direction = "asc";
-      sort.active = "MailType";
-      this.rowData[0].MailTypeList = data;
+      switch (myType) {
+        case "MailTypeList": {
+          sort.active = "MailType";
+          break;
+        }
+        case "MailType": {
+          sort.active = "CampaignName";
+          break;
+        }
+        case "Campaign": {
+          sort.active = "PhaseName";
+          break;
+        }
+        case "Phase": {
+          sort.active = "MailCode";
+          break;
+        }
+        default: {
+          //statements; 
+          break;
+        }
+      }
     }
 
-    this.rowData[0].MailTypeList = data.sort((a, b) => {
+    var sortedData = data.sort((a, b) => {
       const isAsc = sort.direction === 'asc';
       switch (sort.active) {
         case 'MailType': return compare(a.MailType, b.MailType, isAsc);
+        case 'CampaignName': return compare(a.CampaignName, b.CampaignName, isAsc);
+        case 'PhaseName': return compare(a.PhaseName, b.PhaseName, isAsc);
+        case 'MailCode': return compare(a.MailCode, b.MailCode, isAsc);
         case 'Mailed': return compare(a.Measure.Mailed, b.Measure.Mailed, isAsc);
         case 'Caged': return compare(a.Measure.Caged, b.Measure.Caged, isAsc);
         case 'Quantity': return compare(a.Measure.Quantity, b.Measure.Quantity, isAsc);
@@ -95,8 +189,35 @@ export class ReturnsComponent {
         default: return 0;
       }
     });
+
+    switch (myType) {
+      case "MailTypeList": {
+        sort.active = "MailType";
+        this.rowData[0].MailTypeList = sortedData;
+        break;
+      }
+      case "MailType": {
+        Element.CampaignList = sortedData
+        break;
+      }
+      case "Campaign": {
+        Element.PhaseList = sortedData
+        break;
+      }
+      case "Phase": {
+        Element.MailList = sortedData
+        break;
+      }
+      default: {
+        //statements; 
+        break;
+      }
+    }
+
   }
+
 }
+
 
 function compare(a: string, b: string, isAsc) {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);

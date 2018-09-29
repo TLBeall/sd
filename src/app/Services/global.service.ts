6b1298febd105e072645cab82ee40eb2bb24b87b
Sherif Mankarious <smankarious@sunrisedataservices.com>
2018-09-29 00:31:49 -0400

    update

--- src/app/Services/global.service.ts
+++ src/app/Services/global.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
-import { RootReturns } from '../Models/RootReturns.model';
-import { ClientList } from '../Models/ClientList.model';
+// import { Subject, Observable } from 'rxjs';
+// import { RootReturns } from '../Models/RootReturns.model';
+ import { ClientList } from '../Models/ClientList.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +13,15 @@ export class GlobalService {
   public size_md: number = 992;
   public size_sm: number = 768;
   public size_xs: number = 576;
-  public client: string;
-  public startDate: Date;
-  public endDate: Date;
-  public listowner: number;
-  public listmanager: number;
-  public recency: number;
-  public rootReturns: RootReturns;
-  public clientName: string;
-  public clientArr:ClientList[];
+  // public client: string;
+  // public startDate: Date;
+  // public endDate: Date;
+  // public listowner: number;
+  // public listmanager: number;
+  // public recency: number;
+  // public rootReturns: RootReturns;
+  // public clientName: string;
+ public clientArr:ClientList[];
   public clearCurCache: boolean = false;
 
   constructor() {

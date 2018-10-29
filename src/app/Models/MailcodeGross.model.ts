7504e01f5052bbc0a2867c31f932b06e04b002a8
Tyler Beall <tbeall@HSP.hspdirect.com>
2018-10-29 15:12:32 -0400

    added list and phase gross pages

---
+++ src/app/Models/MailcodeGross.model.ts
@@ -0,0 +1,12 @@
+export class MailcodeGross {
+    CardDonors: number;
+    CardGross: number;
+    CashDonors: number;
+    CashGross: number;
+    CheckDonors: number;
+    CheckGross: number;
+    MailCode: string; 
+    NonDonors: number;
+    TotalDonors: number;
+    TotalGross: number;
+}
---
+++ src/app/Models/PhaseGross.model.ts
@@ -0,0 +1,7 @@
+import { ListGross } from "./ListGross.model";
+import { MailcodeGross } from "./MailcodeGross.model";
+
+export class PhaseGross {
+    dateGrossList: ListGross[];
+    mailCodeList: MailcodeGross[];
+}

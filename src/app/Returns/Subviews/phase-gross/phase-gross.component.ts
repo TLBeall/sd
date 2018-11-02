e65a4fa4a2303e901b7260757890cb449d3c8f40
Tyler Beall <tbeall@HSP.hspdirect.com>
2018-11-02 16:47:39 -0400

    changes

--- src/app/Returns/Subviews/phase-gross/phase-gross.component.ts
+++ src/app/Returns/Subviews/phase-gross/phase-gross.component.ts
@@ -6,6 +6,7 @@ import { PhaseGross } from 'src/app/Models/PhaseGross.model';
 import { ListGross } from 'src/app/Models/ListGross.model';
 import { trigger, state, transition, animate, style } from '@angular/animations';
 import { tryParse } from 'selenium-webdriver/http';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-phase-gross',
@@ -29,14 +30,20 @@ export class PhaseGrossComponent implements OnInit {
   private dateTableVis: boolean = true;
   private mailcodeTableVis: boolean = false;
   private rowExpanded: boolean = false;
+  private CurrentTab: string = "date";
+  private allExpanded: boolean = false;
+
 
   dateColumns: string[] = ['Date', 'NonDonors', 'CashDonors', 'CashGross', 'CardDonors', 'CardGross', 'CheckDonors', 'CheckGross', 'TotalDonors', 'TotalGross'];
   mailCodeColumns: string[] = ['Expand', 'MailCode', 'Description', 'NonDonors', 'CashDonors', 'CashGross', 'CardDonors', 'CardGross', 'CheckDonors', 'CheckGross', 'TotalDonors', 'TotalGross'];
 
+  
 
   constructor(private _authService: AuthService, route: ActivatedRoute, private _g: GlobalService) {
     this.route = route;
   }
+
+ 
 
   getTotalValue(measure: string) {
     return this.PhaseGross.dateGrossList.map(t => t[measure]).reduce((acc, value) => acc + value, 0);
@@ -52,35 +59,17 @@ export class PhaseGrossComponent implements OnInit {
       this.LoadValues(params['packagecode'], params['phasenumber']);
 
       this._authService.getPhaseGross(this.PackageCode, this.PhaseNumber)
-        .subscribe(data => {
-          data.dateGrossList = this.sortByStartDate(data.dateGrossList);
-          data.mailCodeList.forEach(element => {
+        .subscribe(PhaseList => {
+          PhaseList.dateGrossList = this.sortByStartDate(PhaseList.dateGrossList);
+          PhaseList.mailCodeList.forEach(element => {
             element.dateGrossList = this.sortByStartDate(element.dateGrossList);
           });
-          this.PhaseGross = data;
+          this.PhaseGross = PhaseList;
           this.pageReady = true;
         });
     });
   }
 
-  CollapseListBtn(Element): boolean {
-    if (Element["Expanded"])
-    {
-      if (Element.Expanded == true) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    else
-      Element["Expanded"] = false;
-      return false;
-  }
-
-  ToggleExpansion(Element: any) {
-      Element.Expanded = !Element.Expanded;
-  }
-  
   LoadValues(packagecode: string, phaseNumber: string) {
     this.PackageCode = packagecode;
     this.PhaseNumber = phaseNumber;
@@ -96,9 +85,67 @@ export class PhaseGrossComponent implements OnInit {
     });
   } 
   
-  tableVisibility(){
-      this.dateTableVis = !this.dateTableVis;
-      this.mailcodeTableVis = !this.mailcodeTableVis;
+  CollapseListBtn(Element): boolean {
+    if (Element["Expanded"])
+    {
+      if (Element.Expanded == true) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    else
+      Element["Expanded"] = false;
+      return false;
   }
 
+  ToggleExpansion(Element: any, ParentList: any[]) {
+      Element.Expanded = !Element.Expanded;
+      this.RunCheck(ParentList);
+  }
+
+  ExpandAllMC(Parent: any, ChildList: any[]){
+    if (Parent.mailCodeList != null){
+    if (this.allExpanded == false){
+      ChildList.forEach(a => {
+        a.Expanded = true;
+      })
+      this.allExpanded = true;
+    } else {
+      ChildList.forEach(a => {
+        a.Expanded = false;
+      })
+      this.allExpanded = false;
+    }
+    }
+  }
+
+  RunCheck(ParentList: any){
+    var i = 0;
+    var parentCount = ParentList.length;
+    ParentList.forEach(a =>{
+      if (a.Expanded == true){
+        i++;
+      } 
+    })
+    if (parentCount == i){
+      this.allExpanded = true;
+    } else {
+      this.allExpanded = false;
+    }
+  }
+
+  tableVisibility(value: any){
+    if (value == "mailcode" && this.CurrentTab =="date"){
+          this.dateTableVis = !this.dateTableVis;
+          this.mailcodeTableVis = !this.mailcodeTableVis;
+          this.CurrentTab = "mailcode";
+    } else if (value == "date" && this.CurrentTab == "mailcode"){
+          this.dateTableVis = !this.dateTableVis;
+          this.mailcodeTableVis = !this.mailcodeTableVis;
+          this.CurrentTab = "date";
+    }
+  }
+
+  
 }

13caa7cb6bdb23426e549ff1e74e17e6d45be69e
Tyler <tbeall@sunrisedataservices.com>
2018-12-21 16:32:10 -0500

    updated year

--- src/app/Utilities_Pages/Caging/caging-calendar/caging-calendar.component.ts
+++ src/app/Utilities_Pages/Caging/caging-calendar/caging-calendar.component.ts
@@ -44,7 +44,8 @@ export class CagingCalendarComponent implements OnInit {
   private totalGross: number;
 
   private elementSelected = false;
-  private selectedArr: CagingCalendarElement[] = [];
+  // private selectedArr: CagingCalendarElement[] = [];
+  private selectedCount: number;
 
   private monthArr: CagingCalendarElement[];
   private monthElement: CagingCalendarElement;
@@ -96,7 +97,7 @@ export class CagingCalendarComponent implements OnInit {
       }
       this.monthArr.push(monthElem);
     }
-    this.calculateSummary(this.monthArr);
+    this.calculateAllSummary();
 
     this.quarterArr = [];
     this.quarters.forEach((element, index) => {
@@ -140,53 +141,6 @@ export class CagingCalendarComponent implements OnInit {
       }
     });
 
-
-    // let Q1 = new CagingQuarterElement;
-    // Q1.Quarter = "Q1";
-    // Q1.selected = false;
-    // let Q2 = new CagingQuarterElement;
-    // Q2.Quarter = "Q2";
-    // Q2.selected = false;
-    // let Q3 = new CagingQuarterElement;
-    // Q3.Quarter = "Q3";
-    // Q3.selected = false;
-    // let Q4 = new CagingQuarterElement;
-    // Q4.Quarter = "Q4";
-    // Q4.selected = false;
-    // this.quarterArr = [Q1, Q2, Q3, Q4];
-    // this.quarterArr.forEach(element => {
-    //   switch (element.Quarter) {
-    //     case "Q1":
-    //       element.Donors = this.monthArr[0].Donors + this.monthArr[1].Donors + this.monthArr[2].Donors;
-    //       element.DonationAmount = this.monthArr[0].DonationAmount + this.monthArr[1].DonationAmount + this.monthArr[2].DonationAmount;
-    //       element.NonDonors = this.monthArr[0].NonDonors + this.monthArr[1].NonDonors + this.monthArr[2].NonDonors;
-    //       element.PieceCount = this.monthArr[0].PieceCount + this.monthArr[1].PieceCount + this.monthArr[2].PieceCount;
-
-    //       break;
-    //     case "Q2":
-    //       element.Donors = this.monthArr[3].Donors + this.monthArr[4].Donors + this.monthArr[5].Donors;
-    //       element.DonationAmount = this.monthArr[3].DonationAmount + this.monthArr[4].DonationAmount + this.monthArr[5].DonationAmount;
-    //       element.NonDonors = this.monthArr[3].NonDonors + this.monthArr[4].NonDonors + this.monthArr[5].NonDonors;
-    //       element.PieceCount = this.monthArr[3].PieceCount + this.monthArr[4].PieceCount + this.monthArr[5].PieceCount;
-    //       element.selected = false;
-    //       break;
-    //     case "Q3":
-    //       element.Donors = this.monthArr[6].Donors + this.monthArr[7].Donors + this.monthArr[8].Donors;
-    //       element.DonationAmount = this.monthArr[6].DonationAmount + this.monthArr[7].DonationAmount + this.monthArr[8].DonationAmount;
-    //       element.NonDonors = this.monthArr[6].NonDonors + this.monthArr[7].NonDonors + this.monthArr[8].NonDonors;
-    //       element.PieceCount = this.monthArr[6].PieceCount + this.monthArr[7].PieceCount + this.monthArr[8].PieceCount;
-    //       element.selected = false;
-    //       break;
-    //     case "Q4":
-    //       element.Donors = this.monthArr[9].Donors + this.monthArr[10].Donors + this.monthArr[11].Donors;
-    //       element.DonationAmount = this.monthArr[9].DonationAmount + this.monthArr[10].DonationAmount + this.monthArr[11].DonationAmount;
-    //       element.NonDonors = this.monthArr[9].NonDonors + this.monthArr[10].NonDonors + this.monthArr[11].NonDonors;
-    //       element.PieceCount = this.monthArr[9].PieceCount + this.monthArr[10].PieceCount + this.monthArr[11].PieceCount;
-    //       element.selected = false;
-    //       break;
-    //   }
-    // });
-
     this.displayCalender();
     let id = 0;
     let currMonthId = parseInt(moment().format('M'));
@@ -194,36 +148,118 @@ export class CagingCalendarComponent implements OnInit {
     this.days = [];
     this.displayCalender();
     this.divideIntoWeek();
-
-
   }
 
+
   selectMonthElement(e: CagingCalendarElement) {
-    if (this.selectedArr.length == 1) {
-      if (e.selected == true) {
-        e.selected = false;
-        this.selectedArr = this.selectedArr.filter(e => e.selected == true);
-        this.calculateSummary(this.monthArr);
-      } else {
-        e.selected = true;
-        this.selectedArr.push(e);
-        this.calculateSummary(this.selectedArr);
+    e.selected = !e.selected;
+    this.checkQuarter();
+    this.checkMonth();
+  }
+
+
+  selectQuarterElement(e: CagingCalendarElement) {
+    if (e.selected == false) {
+      switch (e.Quarter) {
+        case 1:
+          this.monthArr[0].selected = true;
+          this.monthArr[1].selected = true;
+          this.monthArr[2].selected = true;
+          break;
+        case 2:
+          this.monthArr[3].selected = true;
+          this.monthArr[4].selected = true;
+          this.monthArr[5].selected = true;
+          break;
+        case 3:
+          this.monthArr[6].selected = true;
+          this.monthArr[7].selected = true;
+          this.monthArr[8].selected = true;
+          break;
+        case 4:
+          this.monthArr[9].selected = true;
+          this.monthArr[10].selected = true;
+          this.monthArr[11].selected = true;
+          break;
       }
+      e.selected = true;
     } else {
-      if (e.selected == true) {
-        e.selected = false;
-        this.selectedArr = this.selectedArr.filter(e => e.selected == true);
-        this.calculateSummary(this.selectedArr);
-      } else {
-        e.selected = true;
-        this.selectedArr.push(e);
-        this.calculateSummary(this.selectedArr);
+      switch (e.Quarter) {
+        case 1:
+          this.monthArr[0].selected = false;
+          this.monthArr[1].selected = false;
+          this.monthArr[2].selected = false;
+          break;
+        case 2:
+          this.monthArr[3].selected = false;
+          this.monthArr[4].selected = false;
+          this.monthArr[5].selected = false;
+          break;
+        case 3:
+          this.monthArr[6].selected = false;
+          this.monthArr[7].selected = false;
+          this.monthArr[8].selected = false;
+          break;
+        case 4:
+          this.monthArr[9].selected = false;
+          this.monthArr[10].selected = false;
+          this.monthArr[11].selected = false;
+          break;
       }
+      e.selected = false;
+    }
+    this.checkMonth();
+  }
+
+  checkMonth(){
+    this.selectedCount = 0;
+    this.monthArr.forEach(element => {
+      if (element.selected == true) {
+        this.selectedCount++;
+      }
+    });
+
+    if (this.selectedCount == 0) {
+      this.calculateAllSummary();
+    } else {
+      this.calculateSummary();
     }
   }
 
-  selectQuarterElement(e: CagingCalendarElement) {
-    e.selected = !e.selected;
+
+  checkQuarter() {
+    this.quarterArr.forEach(element => {
+      switch (element.Quarter) {
+        case 1:
+          if (this.monthArr[0].selected && this.monthArr[1].selected && this.monthArr[2].selected) {
+            element.selected = true;
+          } else {
+            element.selected = false;
+          }
+          break;
+        case 2:
+          if (this.monthArr[3].selected && this.monthArr[4].selected && this.monthArr[5].selected) {
+            element.selected = true;
+          } else {
+            element.selected = false;
+          }
+          break;
+        case 3:
+          if (this.monthArr[6].selected && this.monthArr[7].selected && this.monthArr[8].selected) {
+            element.selected = true;
+          } else {
+            element.selected = false;
+          }
+          break;
+        case 4:
+          if (this.monthArr[9].selected && this.monthArr[10].selected && this.monthArr[11].selected) {
+            element.selected = true;
+          } else {
+            element.selected = false;
+          }
+          break;
+      }
+    });
   }
 
   // selectQuarterElement(e: CagingQuarterElement) {
@@ -286,13 +322,29 @@ export class CagingCalendarComponent implements OnInit {
   //   });
   // }
 
-  calculateSummary(array: CagingCalendarElement[]) {
+  calculateSummary() {
     this.totalDonors = 0;
     this.totalGross = 0;
     this.totalNonDonors = 0;
     this.totalPieceCount = 0;
 
-    array.forEach(element => {
+    this.monthArr.forEach(element => {
+      if (element.selected == true) {
+        this.totalDonors = this.totalDonors + element.Donors;
+        this.totalGross = this.totalGross + element.DonationAmount;
+        this.totalNonDonors = this.totalNonDonors + element.NonDonors;
+        this.totalPieceCount = this.totalPieceCount + element.PieceCount;
+      }
+    });
+  }
+
+  calculateAllSummary() {
+    this.totalDonors = 0;
+    this.totalGross = 0;
+    this.totalNonDonors = 0;
+    this.totalPieceCount = 0;
+
+    this.monthArr.forEach(element => {
       this.totalDonors = this.totalDonors + element.Donors;
       this.totalGross = this.totalGross + element.DonationAmount;
       this.totalNonDonors = this.totalNonDonors + element.NonDonors;
@@ -310,6 +362,7 @@ export class CagingCalendarComponent implements OnInit {
   getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   }
+
 
 
 
@@ -407,7 +460,7 @@ export class CagingCalendarComponent implements OnInit {
       this.emptyObjects();
       this.displayCalender();
       this.divideIntoWeek();
-    } else { }
+    } else if (this.timeSelection == "Year") { }
   }
   previousMonth() {
     if (this.timeSelection == "Month") {
@@ -415,17 +468,21 @@ export class CagingCalendarComponent implements OnInit {
       this.emptyObjects();
       this.displayCalender();
       this.divideIntoWeek();
-    } else { }
+    } else if (this.timeSelection == "Year") { }
   }
 
   emptyObjects() {
-    this.days = [];
-    this.week1 = [];
-    this.week2 = [];
-    this.week3 = [];
-    this.week4 = [];
-    this.week5 = [];
-    this.week6 = [];
+    if (this.timeSelection == "Month") {
+      this.days = [];
+      this.week1 = [];
+      this.week2 = [];
+      this.week3 = [];
+      this.week4 = [];
+      this.week5 = [];
+      this.week6 = [];
+    } else if (this.timeSelection == "Year") {
+
+    }
   }
 
 
